4d4d0bbde0230934919f59e9e5b9095125a252a0
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-03 23:34:44 +0600

    modals addition done

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AppointmentOption = ({ appointmentOption }) => {
+const AppointmentOption = ({ appointmentOption,setTreatment }) => {
   const {name,slots} = appointmentOption;
   return (
     <div className="card shadow-xl">
@@ -9,7 +9,12 @@ const AppointmentOption = ({ appointmentOption }) => {
         <p>{slots.length > 0 ? slots[0] : 'Try Another Day'}</p>
         <p>{slots.length} {slots.length> 1 ? 'spaces' : 'space'} available</p>
         <div className="card-actions justify-center">
-          <label htmlFor="booking-modal" className="btn btn-primary text-white">Book Appointment</label>
+          <label
+          disabled = {slots.length === 0}
+          onClick={()=>setTreatment(appointmentOption)}
+           htmlFor="booking-modal" 
+           className="btn btn-primary text-white"
+           >Book Appointment</label>
         </div>
       </div>
     </div>
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,6 +1,34 @@
+import { format } from "date-fns";
 import React from "react";
 
-const BookingModal = () => {
+const BookingModal = ({ treatment, selectedDate,setTreatment }) => {
+  const { name, slots } = treatment; //treatment is appointment options just different name here.
+  const date = format(selectedDate, "PP");
+
+  const handleBooking = (event) =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const slot = form.slot.value;
+    const phone = form.phone.value;
+    
+    const booking = {
+      appointmentDate: date,
+      treatment: name,
+      // name: name,
+      patient: name,
+      slot,
+      email,
+      phone,
+    }
+    //send data to the server
+    //and when data is saved then close the modal
+    //then display toast
+
+  console.log(booking);
+  setTreatment(null);
+  }
   return (
     <>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -12,13 +40,49 @@ const BookingModal = () => {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <h3 className="text-lg font-bold">{name}</h3>
+          <form onSubmit={handleBooking} className="grid grid-cols-1 gap-3 mt-10" action="">
+            <input
+              type="text"
+              value={date}
+              disabled
+              className="input w-full  input-bordered"
+            />
+            <select name="slot" className="select select-bordered w-full">
+              
+              {
+                slots.map((slot,i)=> <option 
+                value={slot}>
+                 {slot}
+                 key={i}
+                </option>)
+              }
+            </select>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              className="input w-full input-bordered"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email Address"
+              className="input w-full input-bordered"
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Phone Number"
+              className="input w-full input-bordered"
+            />
+            <br />
+            <input
+              type="submit"
+              className="btn btn-accent w-full "
+              value="submit"
+            />
+          </form>
         </div>
       </div>
     </>
