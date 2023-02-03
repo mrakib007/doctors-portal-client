e32d5407fbc47378572b8cdbcb936884e5a22e89
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-03 11:25:00 +0600

    date picking functionality added

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import chair from '../../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import { format } from "date-fns";
 
-const AppointmentBanner = () => {
+const AppointmentBanner = ({selectedDate,setSelectedDate}) => {
+    
   return (
-    <header>
+    <header className="my-6">
       <div className="hero">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={chair} alt =""
             className="max-w-sm rounded-lg shadow-2xl"
           />
-          <div>
-           
+          <div className="mr-6">
+           <DayPicker
+           mode="single"
+           selected= {selectedDate}
+           onSelect= {setSelectedDate}
+           ></DayPicker>
+           <p>You have selected the date: {format(selectedDate,'PP')}</p>
           </div>
         </div>
       </div>
