68d2198f29760a1ab98c8be88c9b0c9cc9353c37
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-04 12:50:43 +0600

    added context for firebase authentification

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const { register,formState:{errors}, handleSubmit } = useForm();
 
   const handleLogin = data =>{
     
@@ -15,12 +15,19 @@ const Login = () => {
         <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full max-w-xs">
             <label className="label"><span className="label-text">Email</span></label>
-            <input type='text'  className="input input-bordered w-full max-w-xs" {...register("email")}/>
+            <input type='email' 
+             className="input input-bordered w-full max-w-xs" 
+             {...register("email",{required:"Email is required"})}/>
+             {errors.email && <p className="text-red-600">{errors.email?.message}</p>}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label"><span className="label-text">Password</span></label>
-            <input type='password'  className="input input-bordered w-full max-w-xs" {...register("password")}/>
+            <input type='password' 
+             className="input input-bordered w-full max-w-xs" 
+             {...register("password",{required: "Password is required",
+             minLength: {value:6,message:'Password must contain at least 6 characters or longer'}})}/>
             <label className="label"><span className="label-text">Forget Password?</span></label> 
+            {errors.password && <p className="text-red-600">{errors.password?.message}</p>}
           </div>
           <input className="btn btn-accent w-full" value='Login' type="submit" />
         </form>
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
+
+const SignUp = () => {
+    const {register,handleSubmit,formState:{errors}} = useForm();
+    const {createUser} = useContext(AuthContext);
+    const handleSignUp = (data) =>{
+        createUser(data.email,data.password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error=> console.error(error));
+    }
+    return (
+        <div className="h-[800px] flex justify-center items-center">
+        <div className="w-96 p-7">
+          <h2 className="text-xl text-center">Sign Up</h2>
+          <form onSubmit={handleSubmit(handleSignUp)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label"><span className="label-text">Name</span></label>
+              <input type='text' {...register("name",{required:"Name is required"})} className="input input-bordered w-full max-w-xs" />
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label"><span className="label-text">Email</span></label>
+              <input type='email' {...register("email",{required: true})} className="input input-bordered w-full max-w-xs" />
+              {errors.email && <p className='text-red-500'>{errors.email.messages}</p>}
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label"><span className="label-text">Password</span></label>
+              <input type='password' {...register("password",{required:"Password is required",
+              minLength:{value:6,message:"Password must contain 6 characters"},
+              pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+              message: 'Password must have uppercase, number and special characters' }})} 
+              className="input input-bordered w-full max-w-xs" />
+              {errors.password && <p className='text-red-500'>{errors.password.messages}</p>}
+            </div>
+            <input className="btn btn-accent w-full mt-4" value='Sign Up' type="submit" />
+          </form>
+          <p>Already have an account? <Link to="/login" className="text-secondary">Please Login</Link></p>
+          <div className="divider">OR</div>
+          <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+        </div>
+      </div>
+    );
+};
+
+export default SignUp;
