2187a951aeb230d343311c60b54a28ec674b2388
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-09 12:20:25 +0600

    adding add doctor function

--- src/Pages/DashBoard/AddDoctor/AddDoctor.js
+++ src/Pages/DashBoard/AddDoctor/AddDoctor.js
@@ -1,37 +1,92 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useForm } from "react-hook-form";
+import Loading from '../../Shared/Lodading/Loading';
 
 const AddDoctor = () => {
-    const {register,handleSubmit,formState:{errors}} = useForm();
-    const handleAddDoctor = data =>{
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
+  const {data: specialties, isLoading} = useQuery({
+    queryKey: ['specialty'],
+    queryFn: async () =>{
+      const res = await fetch('http://localhost:5000/appointmentSpecialty');
+      const data = await res.json();
+      return data;
     }
-    return (
-        <div className='w-96 p-7'>
-            <h2 className='text-4xl'>Add A Doctor</h2>
-            <form onSubmit={handleSubmit(handleAddDoctor)}>
-            <div className="form-control w-full max-w-xs">
-              <label className="label"><span className="label-text">Name</span></label>
-              <input type='text' {...register("name",{required:"Name is required"})} className="input input-bordered w-full max-w-xs" />
-            </div>
-            <div className="form-control w-full max-w-xs">
-              <label className="label"><span className="label-text">Email</span></label>
-              <input type='email' {...register("email",{required: true})} className="input input-bordered w-full max-w-xs" />
-              {errors.email && <p className='text-red-500'>{errors.email.messages}</p>}
-            </div>
-            <div className="form-control w-full max-w-xs">
-              <label className="label"><span className="label-text">Password</span></label>
-              <input type='password' {...register("password",{required:"Password is required",
-              minLength:{value:6,message:"Password must contain 6 characters"},
-              pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
-              message: 'Password must have uppercase, number and special characters' }})} 
-              className="input input-bordered w-full max-w-xs" />
-              {errors.password && <p className='text-red-500'>{errors.password.messages}</p>}
-            </div>
-            <input className="btn btn-accent w-full mt-4" value='Sign Up' type="submit" />
-          </form>
+  })
+  const handleAddDoctor = (data) => {};
+  if(isLoading){
+    return <Loading></Loading>
+  }
+  return (
+    <div className="w-96 p-7">
+      <h2 className="text-4xl">Add A Doctor</h2>
+      <form onSubmit={handleSubmit(handleAddDoctor)}>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            {...register("name", { required: "Name is required" })}
+            className="input input-bordered w-full max-w-xs"
+          />
+          {errors.name && (
+            <p className="text-red-500">{errors.name.messages}</p>
+          )}
         </div>
-    );
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input input-bordered w-full max-w-xs"
+          />
+          {errors.email && (
+            <p className="text-red-500">{errors.email.messages}</p>
+          )}
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Specialty</span>
+          </label>
+          <select 
+          {...register("specialty")}
+          className="select input-bordered w-full max-w-xs">
+            {
+              specialties.map(specialty => <option
+              key = {specialty._id}
+              value={specialty.name}>{specialty.name}</option>)
+            }
+          </select>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Photo</span>
+          </label>
+          <input
+            type="file"
+            {...register("img", { required: "Image is required" })}
+            className="input input-bordered w-full max-w-xs"
+          />
+          {errors.img && (
+            <p className="text-red-500">{errors.img.messages}</p>
+          )}
+        </div>
+        <input
+          className="btn btn-accent w-full mt-4"
+          value="Add A Doctor"
+          type="submit"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default AddDoctor;
